389d767af327f00854dea95b2087fae8298d8d2c
induilanchezian <induilanchezian@gmail.com>
2017-09-13 06:51:36 +0530

    Confusion Matrix code added

--- lr_1.py
+++ lr_1.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import itertools
 import numpy as np
 import numpy.random as rand
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 import sys
 
 seq2 = pd.Series(np.arange(2))
@@ -29,12 +32,42 @@ def split_train_test(filename, test_ratio):
 	
 
 def train(train_data, train_labels):
-	lr = LogisticRegression()
+	lr = LogisticRegression(class_weight='balanced')
 	lr.fit(train_data,train_labels)	
 	return lr
 	
 def test(test_data, test_labels, lr):
+	test_pred = lr.predict(test_data)
+	cnf_matrix = confusion_matrix(test_labels, test_pred)
+	plt.figure()
+	plot_confusion_matrix(cnf_matrix, classes=seq2, normalize=True,title='Normalized confusion matrix')
+	plt.show()
 	return lr.score(test_data, test_labels)
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 
 if __name__ == "__main__":
